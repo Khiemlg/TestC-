ee67c9c3c7c4a2efcc0f09fe1ba6ef1644b34493
Khiemlg <142194704+Khiemlg@users.noreply.github.com>
2025-05-15 22:08:06 +0700

    demo_test01

---
+++ ConsoleApp1_Solution/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            int a = 50;
+            for (int i = 0; i < a; i++) 
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
